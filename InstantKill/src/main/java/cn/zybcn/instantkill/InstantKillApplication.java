d89fdeab9e40f755601c4d149845e0181689ac28
zhangyibo <849078974@qq.com>
2022-06-18 15:44:52 +0800

    初始化工程项目

---
+++ InstantKill/src/main/java/cn/zybcn/instantkill/InstantKillApplication.java
@@ -0,0 +1,16 @@
+package cn.zybcn.instantkill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangyibo
+ */
+@SpringBootApplication
+public class InstantKillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstantKillApplication.class, args);
+    }
+
+}
---
+++ InstantKill/src/test/java/cn/zybcn/instantkill/InstantKillApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zybcn.instantkill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantKillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
